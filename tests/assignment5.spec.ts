a5f4c93a27be75a4458beed33c1adb84ba1f29f3
Afra Thajudheen <athajudheen@emmes.com>
2026-03-30 11:10:47 +0530

    4th commit

--- tests/assignment5.spec.ts
+++ tests/assignment5.spec.ts
@@ -1,15 +1,17 @@
 import { test, expect } from '@playwright/test';
 import { practiceForm } from '../POM/practiceForm.page';
 import testData from '../test-data/assignment5Details.json';
+import practiceFormDetails from '../test-data/practiceFormDetails.json';
 
 testData.forEach((data) => {
   test(`Form submission test for ${data.fName}`, async ({ page }) => {
 
     const formPage = new practiceForm(page);
 
-    await formPage.navigate(
-      'https://demoqa.com/automation-practice-form'
-    );
+    const{url} = practiceFormDetails;
+    await formPage.navigate(url);
+
+    //await formPage.navigate('https://demoqa.com/automation-practice-form');
 
     await formPage.fillTheForm(
       data.fName,
